5a9a479d405773c5c3367b638248a3beda35dff0
Michael Sevestre <michael@design2code.ca>
2021-05-31 12:47:37 -0400

    Add missing import

--- rakefile.rb
+++ rakefile.rb
@@ -1,4 +1,5 @@
 require_relative 'scripts/utils'
+require_relative 'scripts/R'
 
 task :prepare_for_build, [:build_version] do |t, args|
   update_package_version(args.build_version, description_file)
