103e27f5073c5438d682a261efde20322634e399
Sergei Vavilov <svavil@gmail.com>
2023-02-22 17:09:45 +0100

    debugging with browser()

--- R/ParameterIdentification.R
+++ R/ParameterIdentification.R
@@ -347,6 +347,7 @@ ParameterIdentification <- R6::R6Class(
         x$maxValue
       }), use.names = FALSE)
 
+      browser()
       SAresults <- GenSA::GenSA(par = startValues, fn = private$.targetFunction, lower = lower, upper = upper, control = list(max.time = 60))
       #results <- FME::modFit(f = private$.targetFunction, p = startValues, lower = lower, upper = upper, method = "SANN")
 	    #results <- FME::modFit(f = private$.targetFunction, p = startValues, lower = lower, upper = upper, method = "Nelder-Mead", control = list(maxit = 50))
