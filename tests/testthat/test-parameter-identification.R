e403808bfc230cfa8f3a763422af415e901da4a0
rengelke <rudolf.engelke@web.de>
2024-03-01 14:20:18 +0100

    move simulation and data loading for unit tests to setup file

--- tests/testthat/test-parameter-identification.R
+++ tests/testthat/test-parameter-identification.R
@@ -1,37 +1,4 @@
 
-
-### Testing the parameter identification package on aciclovir example
-simulations <- c(loadSimulation(system.file("extdata", "Aciclovir.pkml", package = "ospsuite")))
-names(simulations) <- "Aciclovir"
-
-piConfiguration <- PIConfiguration$new()
-piConfiguration$printEvaluationFeedback <- FALSE
-
-parameterPaths <- c("Aciclovir|Lipophilicity")
-parameters <- list()
-for (parameterPath in parameterPaths) {
-  modelParams <- list()
-  for (simulation in simulations) {
-    modelParams <- c(modelParams, ospsuite::getParameter(path = parameterPath, container = simulation))
-  }
-  piParameter <- PIParameters$new(parameters = modelParams)
-  parameters <- c(parameters, piParameter)
-}
-parameters[[1]]$minValue <- -10
-parameters[[1]]$maxValue <- 10
-
-filePath <- testthat::test_path("../data/AciclovirLaskinData.xlsx")
-dataConfiguration <- createImporterConfigurationForFile(filePath = filePath)
-dataConfiguration$sheets <- "Laskin 1982.Group A"
-dataConfiguration$namingPattern <- "{Source}.{Sheet}"
-observedData <- loadDataSetsFromExcel(xlsFilePath = filePath, importerConfigurationOrPath = dataConfiguration)
-
-outputMapping <- PIOutputMapping$new(quantity = getQuantity("Organism|PeripheralVenousBlood|Aciclovir|Plasma (Peripheral Venous Blood)",
-  container = simulations$Aciclovir
-))
-outputMapping$addObservedDataSets(observedData$`AciclovirLaskinData.Laskin 1982.Group A`)
-outputMappings <- c(outputMapping)
-
 test_that("A parameter identification object is correctly created", {
   expect_no_error(task <- ParameterIdentification$new(
     simulations = simulations,
