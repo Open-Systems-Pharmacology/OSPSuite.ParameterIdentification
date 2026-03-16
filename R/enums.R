037bb5e8ed88fe3514f4285bd33cf373fe09e22a
rengelke <rengelke@proton.me>
2026-03-16 12:28:41 +0100

    move option specs to option-specs.R

--- R/enums.R
+++ R/enums.R
@@ -157,31 +157,6 @@ CIDefaults <- list(
   bootstrap = CIOptions_bootstrap
 )
 
-#' @noRd
-CIOptionSpecs <- list(
-  hessian = list(
-    epsilon = numericOption(
-      min = .Machine$double.eps,
-      nullAllowed = TRUE,
-      expectedLength = 1
-    ),
-    confLevel = numericOption(min = 0, max = 1)
-  ),
-  PL = list(
-    epsilon = numericOption(
-      min = .Machine$double.eps,
-      nullAllowed = TRUE,
-      expectedLength = NULL
-    ),
-    confLevel = numericOption(min = 0, max = 1),
-    maxIter = integerOption(min = 1L)
-  ),
-  bootstrap = list(
-    nBootstrap = integerOption(min = 1L),
-    confLevel = numericOption(min = 0, max = 1),
-    seed = integerOption(nullAllowed = TRUE)
-  )
-)
 
 #' Objective Function Options for Model Fit Assessment
 #'
@@ -215,21 +190,6 @@ ObjectiveFunctionOptions <- ospsuite.utils::enum(list(
   linScaleCV = 0.2,
   logScaleSD = sqrt(log(1 + 0.2^2, base = 10) / log(10))
 ))
-
-#' @noRd
-ObjectiveFunctionSpecs <- list(
-  objectiveFunctionType = characterOption(allowedValues = c("lsq", "m3")),
-  residualWeightingMethod = characterOption(
-    allowedValues = c("none", "std", "mean", "error")
-  ),
-  robustMethod = characterOption(
-    allowedValues = c("none", "huber", "bisquare")
-  ),
-  scaleVar = logicalOption(),
-  scaling = characterOption(allowedValues = c("lin", "log")),
-  linScaleCV = numericOption(min = 1e-9, max = 1),
-  logScaleSD = numericOption(min = 1e-9)
-)
 
 #' Scaling Options for Output Mapping
 #'
